952388c6610982843397261ed91fa4022ac305ba
Sokol <Sokol@Sokol-PC>
2017-07-23 22:01:13 +0400

    create new project

---
+++ Git-test-project/src/ru/syk/gittest/Main.java
@@ -0,0 +1,10 @@
+package ru.syk.gittest;
+
+public class Main {
+
+	public static void main(String args[]) {
+		// TODO Автоматически созданная заглушка метода
+		System.out.println("Hello Git");
+	}
+
+}
