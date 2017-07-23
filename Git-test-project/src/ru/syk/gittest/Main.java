f8b9bfa8bfd5ed9f851b2cf83f0d2393322251b5
Sokol <Sokol@Sokol-PC>
2017-07-23 22:23:17 +0400

    create proc

--- Git-test-project/src/ru/syk/gittest/Main.java
+++ Git-test-project/src/ru/syk/gittest/Main.java
@@ -3,8 +3,9 @@ package ru.syk.gittest;
 public class Main {
 
 	public static void main(String args[]) {
-		// TODO Автоматически созданная заглушка метода
+		
 		System.out.println("Hello Git");
+		System.out.println("Hello Git11");
 	}
 
 }
